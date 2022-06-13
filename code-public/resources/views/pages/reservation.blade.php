2d19d2356ee9677027c766f53db804779aae3687
tabityousra <92039920+tabityousra@users.noreply.github.com>
2022-06-13 12:50:54 +0100

    add

---
+++ code-public/resources/views/pages/reservation.blade.php
@@ -0,0 +1,154 @@
+@include('layouts.header')
+<section class="page_header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="text-uppercase">Reservation</h2>
+                <p>Tomato is a delicious restaurant website template</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--catégorie-->
+<section class="reservation">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header wow fadeInDown">
+                    <h1>Les Catégories<small>Choisir la catégorie que vous voullez.</small></h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 text-center">
+                <div class="team-staff wow fadeInUp" data-wow-delay="0.2s">
+                    <a href="reservation-forme.html"><img src="img/team/1.png" class="img-responsive center-block" alt="" /></a>
+                    <h4>Enter Name Here</h4>
+                    <p>Designation here</p>
+                    <ul class="team-social">
+                        <li><a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <div class="team-staff wow fadeInUp" data-wow-delay="0.4s">
+                    <a href="reservation-forme.html"><img src="img/team/2.png" class="img-responsive center-block" alt="" /></a>
+                    <h4>Enter Name Here</h4>
+                    <p>Designation here</p>
+                    <ul class="team-social">
+                        <li><a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <div class="team-staff wow fadeInUp" data-wow-delay="0.6s">
+<a href="reservation-forme.html"><img src="img/team/3.png" class="img-responsive center-block" alt="" /></a>
+                    <h4>Enter Name Here</h4>
+                    <p>Designation here</p>
+                    <ul class="team-social">
+                        <li><a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <div class="team-staff wow fadeInUp" data-wow-delay="0.8s">
+                    <a href="reservation-forme.html"><img src="img/team/4.png" class="img-responsive center-block" alt="" /></a>
+                    <h4>Enter Name Here</h4>
+                    <p>Designation here</p>
+                    <ul class="team-social">
+                        <li><a href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- subscribe -->
+<section class="subscribe">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Subscribe</h1>
+                <p>Get updates about new dishes and upcoming events</p>
+                <form class="form-inline" action="php/subscribe.php" id="invite" method="POST">
+                    <div class="form-group">
+                        <input class="e-mail form-control" name="email" id="address" type="email" placeholder="Your Email Address" required>
+                    </div>
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-angle-right"></i>
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer-->
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <h1>About us</h1>
+                <p>Duis leo justo, condimentum at purus eu,Aenean sed dolor sem. Etiam massa libero, auctor vitae egestas et, accumsan quis nunc.Duis leo justo, condimentum at purus eu, posuere pretium tellus.</p>
+                <a href="./about.html">Read more &rarr;</a>
+            </div>
+            <div class="col-md-4  col-sm-6">
+                <h1>Recent post</h1>
+                <div class="footer-blog clearfix">
+                    <a href="./blog_right_sidebar.html">
+                        <img src="img/thumb8.png" class="img-responsive footer-photo" alt="blog photos">
+                        <p class="footer-blog-text">Hand picked ingredients for our best customers</p>
+                        <p class="footer-blog-date">29 may 2015</p>
+                    </a>
+                </div>
+                <div class="footer-blog clearfix last">
+                    <a href="./blog_right_sidebar.html">
+                        <img src="img/thumb9.png" class="img-responsive footer-photo" alt="blog photos">
+                        <p class="footer-blog-text">Daily special foods that you will going to love</p>
+                        <p class="footer-blog-date">29 may 2015</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4  col-sm-6">
+                <h1>Reach us</h1>
+                <div class="footer-social-icons">
+                    <a href="http://www.facebook.com">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href="http://www.twitter.com">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="http://plus.google.com">
+                        <i class="fa fa-google"></i>
+                    </a>
+                    <a href="http://www.youtube.com">
+                        <i class="fa fa-youtube-play"></i>
+                    </a>
+                    <a href="http://www.vimeo.com">
+                        <i class="fa fa-vimeo"></i>
+                    </a>
+                    <a href="http://www.pinterest.com">
+                        <i class="fa fa-pinterest-p"></i>
+                    </a>
+                    <a href="http://www.linkedin.com">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                </div>
+                <div class="footer-address">
+                    <p><i class="fa fa-map-marker"></i>28 Seventh Avenue, Neew York, 10014</p>
+                    <p><i class="fa fa-phone"></i>Phone: (415) 124-5678</p>
+                    <p><i class="fa fa-envelope-o"></i>support@restaurant.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('layouts.footer')
--- code-public/routes/web.php
+++ code-public/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('pages.index');
+});
+Route::get('/reservation.forme', function () {
+    return view('pages.reservation.forme');
+});
+Route::get('/reservation', function () {
+    return view('pages.reservation');
 });
